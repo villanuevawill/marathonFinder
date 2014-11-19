c39285dd8cab52d01ba398e1fd6d86f3992f2cce
Will <william.j.villanueva@gmail.com>
2014-11-18 23:45:44 -0800

    Changed syntax

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var express = require("express"),
-
 	http = require("http");
 
 var app = express();
-
 app.get('/', function(request, response){
 	response.writeHead(200, {"Content-type": "text/plain"});
 	response.end("Hello world!\n");
