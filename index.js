9c8d488c6a21d1bf2e000a20b7771b9db0b0ea5d
Will <william.j.villanueva@gmail.com>
2014-12-04 22:03:47 -0800

    submitting sample file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require("express"),
 
 var app = express();
 app.get('/', function(request, response){
-	response.writeHead(200, {"Content-type": "text/plain"});
-	response.end("Hello world!\n");
+	response.writeHead(200, {"Content-type": "text/html"});
+	response.end("<html> <head><title>hello!</title></head><body><p>hello world!</p></body> </html>");
 });
 http.createServer(app).listen(8000);
